279c08418f83851689126f3a1cec75700e6e970e
Yoann Mosteiro <ymosteirovazquez@asulab.ch>
2021-10-18 16:30:16 +0200

    :white_check_mark: Testing framework

--- inupdater/tests/test_config.py
+++
@@ -1,7 +0,0 @@
-"""Config Test File"""
-import pytest
-
-
-class TestSettings:
-    def test_pass(self):
-        assert True
--- inupdater/tests/test_updater.py
+++
@@ -1,7 +0,0 @@
-"""Updater Test File"""
-import pytest
-
-
-class TestUpdater:
-    def test_pass(self):
-        assert True
